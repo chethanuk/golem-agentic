f86fc4bdb54a643af9ee12b1a4ae4e8381562bb0
Afsal Thaj <afsal.taj06@gmail.com>
2025-08-01 16:46:59 +1000

    try publishing

--- golem-ts-sdk/src/agent-name.ts
+++ golem-ts-sdk/src/agent-name.ts
@@ -5,3 +5,9 @@ export type AgentName = Branded<string, "AgentName">;
 export function createAgentName(name: string): AgentName {
     return name as AgentName;
 }
+
+export type AgentClassName = Branded<string, "AgentClassName">;
+
+export function createAgentClassName(name: string): AgentClassName {
+    return name as AgentClassName;
+}
