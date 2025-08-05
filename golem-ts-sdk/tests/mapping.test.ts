717c4519b035b46be9792d93f780acfe1139c006
Afsal Thaj <afsal.taj06@gmail.com>
2025-08-05 11:51:59 +1000

    set up unit tests for supporting types

--- golem-ts-sdk/tests/mapping.test.ts
+++ golem-ts-sdk/tests/mapping.test.ts
@@ -1,19 +1,42 @@
 import { describe, it, expect } from 'vitest'
 import {Metadata} from "../src";
 import {metadataCollection} from "../.metadata/metadata.index";
+import {PackageName, updateMetadata} from "../src/type_metadata";
+import {constructWitTypeFromTsType} from "../src/mapping/type-mapping";
+import {constructAnalysedTypeFromTsType} from "../src/mapping/type-mapping";
 
-describe('Math test', () => {
-    Metadata.clearMetadata("@golemcloud/golem-ts-sdk");
-
+describe('Test tyes', () => {
+    // Load metadata is simulating the idea of user code updating the base type metadata library
+    // of SDK
     loadMetadata();
-    console.log(Metadata.getTypes().filter((type) => type.name === 'TestType'));
-    it('adds numbers', () => {
+
+    const testTypes = getTestTypes();
+
+    console.log(testTypes);
+
+    // This should be a round trip test
+    it('every type in supported test is successfully converted to Wit Type', () => {
+        testTypes.forEach((type) => {
+            const witType = constructWitTypeFromTsType(type);
+            expect(witType).toBeDefined();
+        });
         expect(1 + 1).toBe(2)
+    })
+
+    it('every interface type can be successfully converted to analysed type of record', () => {
+        testTypes.forEach((type) => {
+            const analysed = constructAnalysedTypeFromTsType(type);
+            expect(analysed).toBeDefined();
+            expect(analysed.kind).toBe('record');
+        });
     })
 })
 
 
 function loadMetadata() {
-   Metadata.clearMetadata("@golemcloud/golem-ts-sdk");
-   metadataCollection.forEach(mod => mod.add(Metadata, false));
+    updateMetadata(metadataCollection)
+}
+
+function getTestTypes() {
+    return Metadata.getTypes().filter((type) => type.module.id == `@@golemcloud/golem-ts-sdk/tests/test-types`);
 }
